934f181fb7e0b5a6417ac5b63d11aa5498d45de9
matheushf <hffmatheus@gmail.com>
2017-07-13 00:21:45 -0300

    adjusts

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
-import {Header} from './components/common';
+import {Header, Button, CardSection, Spinner} from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = {loggedIn: null};
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyDOvGSgj3zkaaSrIcUdqKqNI7y5pvIVZXg',
@@ -13,14 +15,49 @@ class App extends Component {
             projectId: 'authentication-cb3f3',
             storageBucket: 'authentication-cb3f3.appspot.com',
             messagingSenderId: '167410328272'
-        })
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({loggedIn: true});
+            } else {
+                this.setState({loggedIn: false});
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}> Log Out </Button>
+                    </CardSection>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large"/>
+        }
+
+        if (this.state.loggedIn) {
+            return (
+                <CardSection>
+                    <Button>
+                        Log Out
+                    </Button>
+                </CardSection>
+            );
+        }
+
+        return <LoginForm />;
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
