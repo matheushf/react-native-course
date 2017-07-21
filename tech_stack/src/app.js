a7c6d922148434f82d272fc678e697cf30f9c7fe
matheushf <hffmatheus@gmail.com>
2017-07-20 22:31:35 -0300

    components

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Ṕrovider } from 'react-redux';
-import {createStore} from 'redux';
+import { View } from 'react-native';
+import { Ṕrovider } from 'react-redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
     <Provider store={createStore(reducers)}>
-        <View />
+        <View>
+            <Header headerText="Tech Stack" />
+        </View>
     </Provider>
 }
 
